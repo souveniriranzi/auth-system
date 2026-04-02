c0d65d929566d0ac0a30c631bb0144acfd06a8bc
Souvenir Iranzi <isouvenir345@gmail.com>
2026-04-02 18:02:55 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ connectDB();
 app.use(cors());
 app.use(bodyParser.json());           // parses JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // parses URL-encoded bodies
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use("/api/auth", require("./auth"));
 
